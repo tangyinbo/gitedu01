d6373fc92a098fa36076e5921e6767a076af37fe
tangyinbo <tangyinbo@seebon.com>
2018-04-22 18:57:07 +0800

    third commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 			System.out.println("hello world");
 			System.out.println("see you tomorrw")
+			System.out.println("see you day after tomorrow2");
 	}
 }
